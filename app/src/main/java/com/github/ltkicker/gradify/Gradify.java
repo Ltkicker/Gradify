335c00cc4d845b1f6f1a2856202171ea80f60614
CedricRev <53487258+CedricRev@users.noreply.github.com>
2023-05-27 21:58:36 +0800

    Fixed build errors

--- app/src/main/java/com/github/ltkicker/gradify/Gradify.java
+++ app/src/main/java/com/github/ltkicker/gradify/Gradify.java
@@ -9,12 +9,16 @@ public class Gradify extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+
+        // Sets the activity to loading screen
         setContentView(R.layout.activity_loading);
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+
+        // Sets the activity to authentication area
         setContentView(R.layout.activity_authportal);
 
     }
