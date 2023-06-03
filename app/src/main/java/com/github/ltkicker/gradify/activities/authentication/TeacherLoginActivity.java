b65ffd7af5b469e4b7da2e735a163ce38e4abc4f
CedricRev <53487258+CedricRev@users.noreply.github.com>
2023-06-03 18:45:23 +0800

    Make Buttons in Auth Portal Clickable

--- app/src/main/java/com/github/ltkicker/gradify/activities/authentication/TeacherLoginActivity.java
+++ app/src/main/java/com/github/ltkicker/gradify/activities/authentication/TeacherLoginActivity.java
@@ -1,4 +1,15 @@
 package com.github.ltkicker.gradify.activities.authentication;
 
-public class TeacherLoginActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.github.ltkicker.gradify.R;
+
+public class TeacherLoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher_login);
+    }
 }
