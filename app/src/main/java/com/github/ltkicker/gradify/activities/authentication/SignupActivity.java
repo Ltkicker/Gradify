648fbcb8d95df22484259aecc4d57dbb50b1b8bf
CedricRev <53487258+CedricRev@users.noreply.github.com>
2023-06-04 02:41:32 +0800

    Added more functions in auth

--- app/src/main/java/com/github/ltkicker/gradify/activities/authentication/SignupActivity.java
+++ app/src/main/java/com/github/ltkicker/gradify/activities/authentication/SignupActivity.java
@@ -7,11 +7,19 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.github.ltkicker.gradify.R;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class SignupActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        TextInputEditText lastName = findViewById(R.id.input_last_name);
+        TextInputEditText firstName = findViewById(R.id.input_first_name);
+        TextInputEditText middleName = findViewById(R.id.input_mddle_name);
+        TextInputEditText suffixName = findViewById(R.id.input_suffix);
+        TextInputEditText email = findViewById(R.id.input_email);
+        TextInputEditText username = findViewById(R.id.input_username);
+        TextInputEditText password = findViewById(R.id.input_password);
     }
 }
