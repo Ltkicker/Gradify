0dd0cfb1d5ea36f6b5da6fcdd9944278a52554b8
ItsAndres26 <andree.maingat@g.msuiit.edu.ph>
2023-06-04 22:35:34 +0800

    Revert "loading page"
    This reverts commit 86f1a35ee70a3bb1d077b9f66d85042ec5dd2689.

--- app/src/main/java/com/github/ltkicker/gradify/homepage.java
+++ app/src/main/java/com/github/ltkicker/gradify/homepage.java
@@ -3,11 +3,6 @@ package com.github.ltkicker.gradify;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.content.Intent;
-import android.view.View;
-import android.widget.ImageButton;
-import android.widget.TextView;
-import android.os.Bundle;
 import android.widget.Button;
 
 public class homepage extends AppCompatActivity {
