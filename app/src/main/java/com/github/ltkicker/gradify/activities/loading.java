5ad2c685a611b41c1438ae07ae56e10fb36b5155
Andree Katherine Maingat <andree.maingat@g.msuiit.edu.ph>
2023-06-05 14:47:38 +0800

    loadingpage

---
+++ app/src/main/java/com/github/ltkicker/gradify/activities/loading.java
@@ -0,0 +1,24 @@
+package com.github.ltkicker.gradify.activities;
+
+        import android.os.Bundle;
+
+        import androidx.appcompat.app.AppCompatActivity;
+
+        import com.github.ltkicker.gradify.R;
+
+public class loading extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+
+        setContentView(R.layout.activity_loadingpage);
+        //ImageButton studentBtn = findViewById(R.id.imgstudent);
+
+
+
+    }
+}
