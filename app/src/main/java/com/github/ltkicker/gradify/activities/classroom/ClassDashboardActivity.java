594894b8f9bf5455ee13bede62e70976647ac2b6
CedricRev <53487258+CedricRev@users.noreply.github.com>
2023-06-07 00:49:43 +0800

    Fixed stuff in classroom dashboard

--- app/src/main/java/com/github/ltkicker/gradify/activities/classroom/ClassDashboardActivity.java
+++ app/src/main/java/com/github/ltkicker/gradify/activities/classroom/ClassDashboardActivity.java
@@ -3,13 +3,25 @@ package com.github.ltkicker.gradify.activities.classroom;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.github.ltkicker.gradify.R;
+import com.github.ltkicker.gradify.fragments.PopupFragment;
 
 public class ClassDashboardActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_emptyclassroom);
+        setContentView(R.layout.activity_classroom);
+        showPopup();
+
+    }
+
+    private void showPopup() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.popuptemp, new PopupFragment());
+        fragmentTransaction.commit();
     }
 }
