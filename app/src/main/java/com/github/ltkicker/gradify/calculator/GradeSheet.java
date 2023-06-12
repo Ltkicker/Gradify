36c432648bec254b451a92e2e898e37792e5f2e2
ItsAndres26 <andree.maingat@g.msuiit.edu.ph>
2023-06-12 21:43:07 +0800

    search bar (student name & ID no.)

--- app/src/main/java/com/github/ltkicker/gradify/calculator/GradeSheet.java
+++ app/src/main/java/com/github/ltkicker/gradify/calculator/GradeSheet.java
@@ -8,10 +8,12 @@ import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.github.ltkicker.gradify.R;
 import com.github.ltkicker.gradify.data.ModelClass;
+import com.github.ltkicker.gradify.data.StudentAdapter;
 import com.google.firebase.auth.FirebaseAuth;
 import android.content.Intent;
 import android.os.Bundle;
@@ -24,10 +26,11 @@ public class GradeSheet extends AppCompatActivity {
     SearchView searchView;
     RecyclerView recyclerView;
     ArrayList<ModelClass> arrayList=new ArrayList<>();
+    ArrayList<ModelClass> searchList;
     String[] studentList=new String[]{};
 
     String[] studentNumber = new String[]{};
-    int[] imgList = new int[]{};
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate (Bundle savedInstanceState) {
@@ -35,14 +38,88 @@ public class GradeSheet extends AppCompatActivity {
         setContentView(R.layout.activity_gradesheet);
         recyclerView=findViewById(R.id.recyclerView);
         searchView=findViewById(R.id.searchView);
-
+        searchView.setIconified(false);
+        searchView.clearFocus();
         for (int i = 0; i < studentList.length; i++) {
             ModelClass modelClass = new ModelClass();
             modelClass.setStudentName(studentList[i]);
             modelClass.setID_number(studentNumber[i]);
-            modelClass.setImg(imgList[i]);
             arrayList.add(modelClass);
 
         }
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(GradeSheet.this);
+        recyclerView.setLayoutManager(layoutManager);
+
+
+        StudentAdapter studentAdapter = new StudentAdapter(GradeSheet.this,arrayList);
+        recyclerView.setAdapter(studentAdapter);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                searchList = new ArrayList<>();
+
+                if (query.length()>0){
+                    for (int i = 0; i <arrayList.size() ;i++){
+                        if (arrayList.get(i).getStudentName().toUpperCase().contains(query.toUpperCase())|| arrayList.get(i).getID_number().toUpperCase().contains(query.toUpperCase())){
+
+                            ModelClass modelClass = new ModelClass();
+                            modelClass.setStudentName(arrayList.get(i).getStudentName());
+                            modelClass.setID_number(arrayList.get(i).getID_number());
+
+                            searchList.add(modelClass);
+                        }
+                    }
+                    RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(GradeSheet.this);
+                    recyclerView.setLayoutManager(layoutManager);
+
+
+                    StudentAdapter studentAdapter = new StudentAdapter(GradeSheet.this,searchList);
+                    recyclerView.setAdapter(studentAdapter);
+                }
+                else {
+                    RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(GradeSheet.this);
+                    recyclerView.setLayoutManager(layoutManager);
+
+
+                    StudentAdapter studentAdapter = new StudentAdapter(GradeSheet.this,arrayList);
+                    recyclerView.setAdapter(studentAdapter);
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                searchList = new ArrayList<>();
+
+                if (newText.length()>0){
+                    for (int i = 0; i <arrayList.size() ;i++){
+                        if (arrayList.get(i).getStudentName().toUpperCase().contains(newText.toUpperCase()) || arrayList.get(i).getID_number().toUpperCase().contains(newText.toUpperCase())){
+
+                            ModelClass modelClass = new ModelClass();
+                            modelClass.setStudentName(arrayList.get(i).getStudentName());
+                            modelClass.setID_number(arrayList.get(i).getID_number());
+
+                            searchList.add(modelClass);
+                        }
+                    }
+                    RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(GradeSheet.this);
+                    recyclerView.setLayoutManager(layoutManager);
+
+
+                    StudentAdapter studentAdapter = new StudentAdapter(GradeSheet.this,searchList);
+                    recyclerView.setAdapter(studentAdapter);
+                }
+                else {
+                    RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(GradeSheet.this);
+                    recyclerView.setLayoutManager(layoutManager);
+
+
+                    StudentAdapter studentAdapter = new StudentAdapter(GradeSheet.this,arrayList);
+                    recyclerView.setAdapter(studentAdapter);
+                }
+                return false;
+            }
+        });
     }
 }
--- app/src/main/java/com/github/ltkicker/gradify/data/StudentAdapter.java
+++ app/src/main/java/com/github/ltkicker/gradify/data/StudentAdapter.java
@@ -1,28 +1,55 @@
 package com.github.ltkicker.gradify.data;
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.github.ltkicker.gradify.R;
+
+import java.util.ArrayList;
+
 public class StudentAdapter extends RecyclerView.Adapter<StudentAdapter.MyHolder> {
+    Context context;
+    ArrayList<ModelClass>arrayList;
+    LayoutInflater layoutInflater;
+
+    public StudentAdapter(Context context, ArrayList<ModelClass> arrayList) {
+        this.context = context;
+        this.arrayList = arrayList;
+        layoutInflater = LayoutInflater.from(context);
+    }
+
     @NonNull
     @Override
     public MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = layoutInflater.inflate(R.layout.activity_itemfileforgradesheet,parent,false);
+        return new MyHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull StudentAdapter.MyHolder holder, int position) {
 
+        holder.StudentName.setText(arrayList.get(position).getStudentName());
+        holder.IDNum.setText(arrayList.get(position).getID_number());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+
+        return arrayList.size();
     }
     public class MyHolder extends RecyclerView.ViewHolder{
+        TextView StudentName,IDNum;
         public MyHolder (@NonNull View itemView){
+
             super(itemView);
+            StudentName = itemView.findViewById(R.id.txt);
+            IDNum = itemView.findViewById(R.id.txt2);
+
         }
     }
 }
