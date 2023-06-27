7a692566a1b4af7a9a285ad059806545517a395f
Cedric Revestir <53487258+CedricRev@users.noreply.github.com>
2023-06-27 10:45:30 +0800

    Revert "Pawix"

--- app/src/main/java/com/github/ltkicker/gradify/data/classrooms/Classroom.java
+++ app/src/main/java/com/github/ltkicker/gradify/data/classrooms/Classroom.java
@@ -40,7 +40,6 @@ public class Classroom {
         this.teacher = teacher;
 
         FirebaseDatabase.getInstance().getReference("grades").child(key).setValue(new GradingSystem());
-        //ang reason ani nga database kay tungod sa usa ka specific classroom, wherein i store sa grades under ana  
     }
 
     public void getData(Classroom classroom) {
