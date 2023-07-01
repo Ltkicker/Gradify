9a9f8e176cd68dd553690de447f294dca695dfb8
Lt <barroshielamae2@gmail.com>
2023-07-01 12:14:25 +0800

    Revert "FINAL SA LOADING"
    This reverts commit 15eb39fdba1ff7b1e0b9026d0e0f55df1dd5f8d5.

--- app/src/main/java/com/github/ltkicker/gradify/activities/Gradify.java
+++ app/src/main/java/com/github/ltkicker/gradify/activities/Gradify.java
@@ -22,10 +22,45 @@ public class Gradify extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity0_main_loading_withlogo);
+        setContentView(R.layout.activity00_allpagesforloading);
 
         FirebaseUtils.init();
 
+        final LinearLayout loadingContainer = findViewById(R.id.loadingContainer);
+        final View loading1 = findViewById(R.id.activity0_main_loading_withlogo);
+        final View loading2 = findViewById(R.id.activity1_main_loading);
+        final View loading3 = findViewById(R.id.activity2_main_loading_withlogo);
+
+        // Hide all loading screens initially, except the first one
+        loading2.setVisibility(View.GONE);
+        loading3.setVisibility(View.GONE);
+
+        // Add a delay before showing the next loading screen
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                loading1.setVisibility(View.GONE);
+                loading2.setVisibility(View.VISIBLE);
+            }
+        }, 5000); // 2000 milliseconds delay
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                loading2.setVisibility(View.GONE);
+                loading3.setVisibility(View.VISIBLE);
+            }
+        }, 10000); // 4000 milliseconds delay
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Gradify.this, AuthPortalActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 10000); // 4000 milliseconds delay
 
     }
 
